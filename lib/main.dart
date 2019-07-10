0f7d3994920cbc3bb8f30c83d9566100e1b5edc1
Karan Singh <karan@singhkaran.com>
2019-07-11 02:31:19 +0530

    Boss Level Challenge 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,42 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue.shade700,
+          appBar: AppBar(
+            backgroundColor: Colors.black87,
+            title: Text('Ask Me Anything!'),
+          ),
+          body: Magic8BallApp(),
+        ),
       ),
     );
+
+class Magic8BallApp extends StatefulWidget {
+  @override
+  _Magic8BallAppState createState() => _Magic8BallAppState();
+}
+
+class _Magic8BallAppState extends State<Magic8BallApp> {
+  int answerNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Center(
+        child: Container(
+          child: FlatButton(
+            onPressed: () {
+              setState(() {
+                answerNumber = Random().nextInt(5) + 1;
+              });
+            },
+            child: Image.asset('images/ball$answerNumber.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
